f5cda7ba1d46d0c9cb512ca1700b010a337612e7
Hai_Nguyen <thanhhai08sk@gmail.com>
2015-10-20 21:11:49 +0700

    create a dummy master/flow activity

--- app/src/main/java/org/de_studio/phonetools/MainFragment.java
+++ app/src/main/java/org/de_studio/phonetools/MainFragment.java
@@ -1,5 +1,6 @@
 package org.de_studio.phonetools;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.MatrixCursor;
@@ -14,6 +15,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.mobeta.android.dslv.DragSortController;
 import com.mobeta.android.dslv.DragSortListView;
@@ -120,6 +122,13 @@ public  class MainFragment extends Fragment implements LoaderManager.LoaderCallb
         mMainAdapter = new MainAdapter(getActivity(),null,0);
         mListView.setAdapter(mMainAdapter);
         mListView.setDropListener(onDrop);
+        Button floatButton = (Button) rootView.findViewById(R.id.floating_button_1);
+        floatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),ItemListActivity.class));
+            }
+        });
 
 
 
