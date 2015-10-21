13516ebbbfaaf4a76b8f7e5de37833a711c393d5
Hai_Nguyen <thanhhai08sk@gmail.com>
2015-10-21 09:42:49 +0700

    add column string to item detail fragment

--- app/src/main/java/org/de_studio/phonetools/ItemDetailFragment.java
+++ app/src/main/java/org/de_studio/phonetools/ItemDetailFragment.java
@@ -14,6 +14,38 @@ public class ItemDetailFragment extends ListFragment {
     ArrayList<String> list;
 
 
+    public static final String[] PHONE_TOOLS_COLUMNS = {
+            PhoneToolsContract.MainEntry.TABLE_NAME + "."+ PhoneToolsContract.ActionEntry._ID,
+            PhoneToolsContract.MainEntry.COLUMN_TYPE,
+            PhoneToolsContract.MainEntry.COLUMN_DESTINATION,
+            PhoneToolsContract.MainEntry.COLUMN_TITLE,
+            PhoneToolsContract.MainEntry.COLUMN_DESCRIPTION,
+            PhoneToolsContract.MainEntry.COLUMN_CARRIER_ID,
+            PhoneToolsContract.MainEntry.TABLE_NAME + "." + PhoneToolsContract.CarriersEntry.COLUMN_CARRIER_NAME,
+            PhoneToolsContract.MainEntry.COLUMN_TEXT,
+            PhoneToolsContract.MainEntry.COLUMN_CANCEL,
+            PhoneToolsContract.MainEntry.COLUMN_MONEY,
+            PhoneToolsContract.MainEntry.COLUMN_CYCLE,
+            PhoneToolsContract.MainEntry.COLUMN_IN_MAIN,
+            PhoneToolsContract.MainEntry.COLUMN_CATEGORY
+
+    };
+
+    static final int COL_MAIN_ID = 0;
+    static final int COL_MAIN_TYPE = 1;
+    static final int COL_MAIN_DESTINATION = 2;
+    static final int COL_MAIN_TITLE = 3;
+    static final int COL_MAIN_DESCRIPTION = 4;
+    static final int COL_MAIN_CARRIER_ID = 5;
+    static final int COL_CARRIERS_CARRIER_NAME = 6;
+    static final int COL_MAIN_TEXT = 7;
+    static final int COL_MAIN_CANCEL = 8;
+    static final int COL_MAIN_MONEY = 9;
+    static final int COL_MAIN_CYCLE = 10;
+    static final int COL_MAIN_IN_MAIN = 11;
+    static final int COL_CATEGORY = 12;
+
+
 
     public ItemDetailFragment() {
     }
@@ -38,17 +70,5 @@ public class ItemDetailFragment extends ListFragment {
 
 
     }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
-//
-//        // Show the dummy content as text in a TextView.
-//        if (mPosition != null) {
-//            ((TextView) rootView.findViewById(R.id.item_detail)).setText("helllllo");
-//        }
-//
-//        return rootView;
-//    }
+
 }
