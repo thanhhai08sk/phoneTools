3fe6e5f0abb93d248c5bf52b1c2639a0e931d06a
Hai_Nguyen <thanhhai08sk@gmail.com>
2015-10-26 08:14:04 +0700

    add dialogFragment and it's layout

---
+++ app/src/main/java/org/de_studio/phonetools/AddItemFragment.java
@@ -0,0 +1,13 @@
+package org.de_studio.phonetools;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by hai on 10/25/2015.
+ */
+public class AddItemFragment extends DialogFragment {
+
+
+
+
+}
