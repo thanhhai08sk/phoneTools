f07408c626a8cbe232b3d6ea566d2a98cc8f6633
Hai_Nguyen <thanhhai08sk@gmail.com>
2015-11-03 16:12:47 +0700

    show tabbar in ServiceDetailActivity

--- app/src/main/java/org/de_studio/phonetools/MainActivity.java
+++ app/src/main/java/org/de_studio/phonetools/MainActivity.java
@@ -26,20 +26,9 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     SectionsPagerAdapter mSectionsPagerAdapter;
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     ViewPager mViewPager;
     private static final int MAIN_LOADER = 0;
     MainFragment mainFragment;
--- app/src/main/java/org/de_studio/phonetools/ServiceDetailActivity.java
+++ app/src/main/java/org/de_studio/phonetools/ServiceDetailActivity.java
@@ -1,13 +1,20 @@
 package org.de_studio.phonetools;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+import java.util.Locale;
 
 public class ServiceDetailActivity extends AppCompatActivity {
+    SectionsPagerAdapter mSectionsPagerAdapter;
+    ViewPager mViewPager;
+    private static final String LOG_TAG = ServiceDetailTabbarFragment.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +22,54 @@ public class ServiceDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_service_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.service_detail_viewPager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.service_detail_tabbar);
+        tabLayout.setupWithViewPager(mViewPager);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position) {
+                case 0:
+                    return  ServiceDetailTabbarFragment.newInstance(position + 1);
+                case 1:
+                    return ServiceDetailTabbarFragment.newInstance(position + 1);
+                case 2:
+                    return ServiceDetailTabbarFragment.newInstance(position +1);
+
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return "Gọi điện-nhắn tin".toLowerCase();
+                case 1:
+                    return "Dịch vụ 3G".toLowerCase();
+                case 2:
+                    return "Tiện ích".toLowerCase();
+
+            }
+            return null;
+        }
     }
 
 }
