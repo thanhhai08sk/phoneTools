baf386fc6883140b8d0d34132d940046a13eb6d5
Hai_Nguyen <thanhhai08sk@gmail.com>
2015-11-09 21:02:56 +0700

    more design add MyPreferenceFragment class

---
+++ app/src/main/java/org/de_studio/phonetools/MyPreferenceFragment.java
@@ -0,0 +1,62 @@
+package org.de_studio.phonetools;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by hai on 11/9/2015.
+ */
+public  class MyPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener{
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    public static MyPreferenceFragment newInstance(int sectionNumber) {
+        MyPreferenceFragment fragment = new MyPreferenceFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    public MyPreferenceFragment() {
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_general);
+        bindPreferenceSummaryToValue(findPreference("carrier"));
+    }
+
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(this);
+
+        // Trigger the listener immediately with the preference's
+        // current value.
+        onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String stringValue = newValue.toString();
+
+        if (preference instanceof ListPreference) {
+            // For list preferences, look up the correct display value in
+            // the preference's 'entries' list (since they have separate labels/values).
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            // For other preferences, set the summary to the value's simple string representation.
+            preference.setSummary(stringValue);
+        }
+
+
+        return true;
+    }
+}
