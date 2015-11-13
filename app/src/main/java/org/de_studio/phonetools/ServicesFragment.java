10ace3074490aac2b47ce6a5c12b21d21e1017bd
Hai_Nguyen <thanhhai08sk@gmail.com>
2015-11-13 20:54:59 +0700

    test Jsoup to get khuyen mai information from vienthong.com.vn website ok

--- app/src/main/java/org/de_studio/phonetools/ServicesFragment.java
+++ app/src/main/java/org/de_studio/phonetools/ServicesFragment.java
@@ -6,19 +6,38 @@ package org.de_studio.phonetools;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ServicesFragment extends Fragment {
+    private static final String LOG_TAG = ServicesFragment.class.getSimpleName();
     ArrayList<String> list;
     private String mCategory;
     public static final String EXTRA_KEY = "category";
     private static final String ARG_SECTION_NUMBER = "section_number";
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            TextView result =(TextView) getView().findViewById(R.id.service_teset_result);
+            String text = msg.getData().getString("result","nothing");
+            result.setText(text);
+        }
+    };
 
     public static ServicesFragment newInstance(int sectionNumber) {
         ServicesFragment fragment = new ServicesFragment();
@@ -60,7 +79,38 @@ public class ServicesFragment extends Fragment {
             }
         });
 
+        Button testButton = (Button) v.findViewById(R.id.service_test_button);
+        final TextView testResult = (TextView) v.findViewById(R.id.service_teset_result);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        String testResultString ="";
+                        try {
+                            String url = "https://vienthong.com.vn/tin-tuc/tin-khuyen-mai/";
+                            Document document = Jsoup.connect(url).get();
+                            Elements elements = document.select("li.clearfix:lt(3) a[href] [title]");
+                            for (Element  element : elements){
+                                String title = element.attr("title");
+                                testResultString = testResultString + title;
+                            }
+                        }catch (IOException e){
+                            Log.e(LOG_TAG,"err get information from website");
+                        }
+                        Bundle bundle = new Bundle();
+                        bundle.putString("result",testResultString);
+                        Message message = new Message();
+                        message.setData(bundle);
+                        handler.sendMessage(message);
+                    }
+                };
+                Thread thread = new Thread(runnable);
+                thread.start();
 
+            }
+        });
         return v;
     }
 
